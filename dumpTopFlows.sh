53b1671b3cd71849235499bea744ce8154823de8
Matthew Wollenweber <mjw@cyberwart.com>
2015-06-02 11:45:16 -0400

    meh

--- dumpTopFlows.sh
+++ dumpTopFlows.sh
@@ -6,7 +6,7 @@
 #configuration
 START=`date --date '-2 hour' "+%Y/%m/%d:%H"`;
 DATA_DIR='/tmp/silkweb'; #changeme
-OUTFILE=`date  "+%Y-%m-%d.%H.csv"`
+OUTFILE=`date  "+%Y-%m-%d.%H.IN.csv"`
 HOME_NET="128.164.0.0/16,161.253.0.0/16"; #changeme
 
 mkdir $DATA_DIR &> /dev/null
@@ -19,4 +19,11 @@ RWFARGS=" --class=all --proto=0-255 --not-dcidr=$HOME_NET --start-date=$START --
 RWSTATS="/usr/bin/rwstats";
 RWSARGS=" --fields=dip --percentage=1  --no-titles"
 
+$RWFILTER $RWFARGS | $RWSTATS $RWSARGS > $DATA_DIR/$OUTFILE
+
+
+
+
+OUTFILE=`date  "+%Y-%m-%d.%H.OUT.csv"`
+RWFARGS=" --class=all --proto=0-255 --not-scidr=$HOME_NET --start-date=$START --pass=stdout ";
 $RWFILTER $RWFARGS | $RWSTATS $RWSARGS > $DATA_DIR/$OUTFILE
